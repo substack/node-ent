630f3c3871429f49e5b67844d5504f80882dccbc
Matt Dunlap <mdunlap@yahoo-inc.com>
2013-05-06 09:46:59 -0700

    Simplified encoding/decoding process by removing unneeded semicolons from entity map

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ exports.encode = function (str) {
     if (typeof str !== 'string') {
         throw new TypeError('Expected a String');
     }
-    
+
     return str.split('').map(function (c) {
         var e = revEntities[c];
         var cc = c.charCodeAt(0);
         if (e) {
-            return '&' + (e.match(/;$/) ? e : e + ';');
+            return '&' + e + ';';
         }
         else if (c.match(/\s/)) {
             return c;
@@ -34,7 +34,7 @@ exports.decode = function (str) {
     if (typeof str !== 'string') {
         throw new TypeError('Expected a String');
     }
-    
+
     return str
         .replace(/&#(\d+);?/g, function (_, code) {
             return String.fromCharCode(code);
@@ -42,12 +42,9 @@ exports.decode = function (str) {
         .replace(/&#[xX]([A-Fa-f0-9]+);?/g, function (_, hex) {
             return String.fromCharCode(parseInt(hex, 16));
         })
-        .replace(/&([^;\W]+;?)/g, function (m, e) {
-            var ee = e.replace(/;$/, '');
-            var target = entities[e]
-                || (e.match(/;$/) && entities[ee])
-            ;
-            
+        .replace(/&([^;\W]+);?/g, function (m, e) {
+            var target = entities[e];
+
             if (typeof target === 'number') {
                 return String.fromCharCode(target);
             }
