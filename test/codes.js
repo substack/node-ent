f64c515c758373ab40d491c1e3b14479c4b335ef
Dan Polivy <dan@cellartracker.com>
2013-08-13 14:59:28 -0700

    Add 'useNamedReferences' encoding option
    Add an optional parameter to encode() that determines whether to use
    named or decimal entities when encoding the string (default: named).
    
    The encoding behavior is now consistent, as follows:
    - Special ASCII characters are always encoded (& < > " ').
    - Control characters (<32) are encoded as decimals.
    - When useNamedReferences is false, extended characters (>= 127) are
    encoded by name when available, and by number otherwise.
    - When useNamedReferences is true, extended characters (>=127) are
    always encoded by number.
    
    Updated readme and tests accordingly.

--- test/codes.js
+++ test/codes.js
@@ -1,6 +1,7 @@
 var test = require('tape');
 var punycode = require('punycode');
 var ent = require('../');
+var revEntities = require('../reversed.json');
 
 test('amp', function (t) {
     var a = 'a & b & c';
@@ -31,7 +32,7 @@ test('num', function (t) {
     var b = '&#1337;';
     t.equal(ent.encode(a), b);
     t.equal(ent.decode(b), a);
-    
+
     t.equal(ent.encode(a + a), b + b);
     t.equal(ent.decode(b + b), a + a);
     t.end();
@@ -49,15 +50,15 @@ test('hex', function (t) {
             t.equal(ent.encode(a), '&#' + i + ';');
         }
     }
-    
+
     for (var i = 127; i < 2000; i++) {
         var a = String.fromCharCode(i);
         var b = '&#x' + i.toString(16) + ';';
         var c = '&#X' + i.toString(16) + ';';
-        
+
         t.equal(ent.decode(b), a);
         t.equal(ent.decode(c), a);
-        
+
         var encoded = ent.encode(a);
         var encoded2 = ent.encode(a + a);
         if (!encoded.match(/^&\w+;/)) {
@@ -73,5 +74,45 @@ test('astral num', function (t) {
     var b = '&#x1d306;';
     t.equal(ent.decode(b), a);
     t.equal(ent.decode(b + b), a + a);
+    t.end();
+});
+
+test('decimal encoding', function (t) {
+    var a = 'a © l π h å ß ĕ t â';
+    var b = 'a &#169; l &#960; h &#229; &#223; &#277; t &#226;';
+    t.equal(ent.encode(a, { useNamedReferences: false }), b);
+    t.equal(ent.decode(b), a);
+
+    for (var key in revEntities) {
+        var e = revEntities[key],
+            a = '&' + e,
+            b = '&#' + key + ';';
+
+        // Don't test standard ASCII characters (they aren't encoded)
+        if (key >= 32 && key < 127) continue;
+
+        // Decode into the Unicode character
+        var decoded = ent.decode(a);
+
+        // Encode it as a decimal entity
+        var encodedDec = ent.encode(decoded, { useNamedReferences: false });
+
+        // Make sure the decimal entity is properly encoded
+        t.equal(encodedDec, b);
+
+        // And decode once more, for good measure
+        t.equal(decoded, ent.decode(encodedDec));
+    }
+
+    t.end();
+});
+
+test('double encoding', function(t) {
+    var a = '&3509;&3510;',
+        b = '&amp;3509;&amp;3510;';
+
+    t.equal(ent.encode(a), b);
+    t.equal(ent.encode(a, { useNamedReferences: false }), b);
+
     t.end();
 });
